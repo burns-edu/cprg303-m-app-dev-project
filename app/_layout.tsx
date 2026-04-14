f4bc85a11dae4a733fee185fd22e68e1ae9005de
burns-edu <ryan.h.burns@edu.sait.ca>
2026-04-13 19:55:42 -0600

    Created header with back and nav buttons (non-functional)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
-import { Stack } from "expo-router";
+import Header from "@/components/header";
+import { Stack, useNavigation } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  const navigation = useNavigation();
+
+  return (
+    <Stack
+      // Header default settings
+      screenOptions={{
+        header: () => <Header showBack={navigation.canGoBack()} showNav />,
+      }}
+    >
+      {/* Login Page - No back or nav buttons */}
+      <Stack.Screen name="index" options={{ header: () => <Header /> }} />
+    </Stack>
+  );
 }
---
+++ components/header.tsx
@@ -0,0 +1,92 @@
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+type HeaderProps = {
+  showBack?: boolean;
+  showNav?: boolean;
+};
+
+export default function Header({
+  showBack = false,
+  showNav = false,
+}: HeaderProps) {
+  return (
+    <View style={styles.header}>
+      {/* Back Button */}
+      <View style={styles.placeholder}>
+        {showBack && (
+          <TouchableOpacity
+            style={styles.buttonBox}
+            onPress={() => console.log("Back pressed")}
+          >
+            <Text style={styles.button}>❮</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+
+      {/* Logo */}
+      <Text style={styles.title}>Bingo Banking</Text>
+
+      {/* Nav Button */}
+      <View style={styles.placeholder}>
+        {showNav && (
+          <TouchableOpacity
+            style={styles.buttonBox}
+            onPress={() => console.log("Nav pressed")}
+          >
+            <Text style={styles.button}>☰</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#7F9C50",
+    paddingTop: StatusBar.currentHeight,
+    paddingBottom: 10,
+    paddingHorizontal: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  title: {
+    flex: 1,
+    paddingLeft: 10,
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#FFFFFF",
+  },
+
+  placeholder: {
+    width: 40,
+    height: 40,
+  },
+
+  buttonBox: {
+    width: 40,
+    height: 40,
+    backgroundColor: "#99B868",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  button: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#FFFFFF",
+    textAlign: "center",
+    textAlignVertical: "center",
+    top: -2,
+  },
+});
